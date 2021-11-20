c250d271c5e334567ded8e1f61ebe5d09c97af20
Joao <joao.teixeira@aedb.br>
2021-11-19 21:58:57 -0300

    adicionando as fontes no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './global/styles/theme';
+import theme from "./global/styles/theme";
 
-import { Dashboard } from './screens/Dashbord';
+import { Dashboard } from "./screens/Dashbord";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if(!fontLoaded) {
+    return <AppLoading/>;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
