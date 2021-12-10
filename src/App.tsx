8d886ba91585c14a5df64fa78e043aaed57b168d
Joao <joao.teixeira@aedb.br>
2021-12-10 16:10:37 -0300

    ajustando o StatusBar da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import { StatusBar } from 'react-native';
 
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
@@ -31,6 +32,10 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar 
+          barStyle="light-content" 
+          backgroundColor={theme.colors.primary}
+        />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
